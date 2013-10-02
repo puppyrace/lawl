50642194a51882a3e0a058c5c2764db4677f4340
Snow Magician <snowmagician@live.com>
2013-10-02 17:47:52 -0400

    .

--- a.b.c5/www/js/index.js
+++ a.b.c5/www/js/index.js
@@ -38,7 +38,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 		
-		var commandresult;		
+		var commandresult = true;		
 		commandresult = alert("Calling Device Ready");
 		
 		if(commandresult) alert("Calling Device Ready Was Good"); else alert("Bad Calling Device READY");
